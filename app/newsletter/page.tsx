8eac36513f091b1e429d58573829648a3e964807
Leandro <leandro.soares.eneterio@gmail.com>
2025-08-11 18:33:44 -0300

    v0.6.0

---
+++ app/newsletter/page.tsx
@@ -0,0 +1,15 @@
+import Coming from '@/components/comming'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Coming 
+            title='Estamos Criando Algo INCRÍVEL'
+            subtitle='A página está em construção — venha dar uma olhada em alguns dias'
+            eta='Desenvolvedor Reponsável'
+            note='https://leandrodevportfolio.vercel.app'
+        />
+  )
+}
+
+export default page
